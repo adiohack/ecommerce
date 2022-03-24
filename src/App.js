88ad7ebf959deafa0fa816ec29e53fa5c79ade53
adiohack <adiohack@gmail.com>
2022-03-24 14:52:16 +0200

    running app added NavBar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { NavBar } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Yellow</h1>
+    </div>
+  );
+}
+
+export default App;
