03c24aae3118f9b35b8fac527a49fbf9a40c5055
adiohack <adiohack@gmail.com>
2022-03-25 21:05:08 +0200

    resolved to fetch data from graphql YEEEEY! EndOfDay2

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import "./styles.css";
 
 class Products extends React.Component {
+  constructor(props) {
+    console.log("Constructor");
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+    fetch("http://localhost:4000/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+      query {
+      categories {
+          products {
+            name 
+            }
+          }
+        }
+      `,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   render() {
-    return <p>Products</p>;
+    console.log("Render");
+    return <div className="products">asd</div>;
   }
 }
 
